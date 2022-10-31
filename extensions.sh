572efccc2856164fe0728b430d5bd7486862840d
emptyhopes <literallymyfault@gmail.com>
2022-10-31 17:38:42 +0000

    base

--- extensions.sh
+++ extensions.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
 
-BaseURL="https://extensions.gnome.org"
-GnomeVersion=$(gnome-shell --version | cut --delimiter=" " --fields="3")
-extensions=("user-theme@gnome-shell-extensions.gcampax.github.com" "dash-to-dock@micxgx.gmail.com" "blur-my-shell@aunetx")
+source "variables.sh"
 
-for extension in ${extensions[@]}; do
-   temporary=$(mktemp --directory)
-   destination="$LocalDirectoryExtensions/$extension"
-   url=$(GetDownloadURLExtension "$extension")
-   curl --silent --location "$url" >"$temporary/$extension.zip"
-   unzip -q "$temporary/$extension.zip" -d "$destination"
-   sudo rm --recursive --force "$temporary"
-   echo "Install $extension: Success."
-done
+DownloadExtensions() {
+   BaseURL="https://extensions.gnome.org"
+   GnomeVersion=$(gnome-shell --version | cut --delimiter=" " --fields="3")
+   extensions=("user-theme@gnome-shell-extensions.gcampax.github.com" "dash-to-dock@micxgx.gmail.com" "blur-my-shell@aunetx")
+
+   for extension in ${extensions[@]}; do
+      temporary=$(mktemp --directory)
+      destination="$LocalDirectoryExtensions/$extension"
+      url=$(GetDownloadURLExtension "$extension")
+
+      curl --silent --location "$url" >"$temporary/$extension.zip"
+      unzip -q "$temporary/$extension.zip" -d "$destination"
+
+      sudo rm --recursive --force "$temporary"
+
+      echo "Install $extension: Success."
+   done
+}
 
 GetInformationExtension() {
    echo "$BaseURL/extension-info/?uuid=${1}&shell_version=$GnomeVersion"
@@ -21,3 +28,5 @@ GetInformationExtension() {
 GetDownloadURLExtension() {
    echo "$BaseURL$(curl --silent --location "$(GetInformationExtension $1)" | sed --expression 's/.*"download_url": "\([^"]*\)".*/\1/')"
 }
+
+DownloadExtensions
---
+++ variables.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+LocalDirectoryExtensions="$HOME/.local/share/gnome-shell/extensions"
+LocalDirectoryBackgrounds="$HOME/.local/share/backgrounds"
+LocalDirectoryThemes="$HOME/.local/share/themes"
+LocalDirectoryCursors="$HOME/.local/share/icons"
+LocalDirectoryIcons="$HOME/.local/share/icons"
+LocalDirectoryFonts="$HOME/.local/share/fonts"
+
+RootDirectoryExtensions="/usr/share/gnome-shell/extensions"
+RootDirectoryBackgrounds="/usr/share/share/backgrounds"
+RootDirectoryThemes="/usr/share/share/themes"
+RootDirectoryCursors="/usr/share/share/icons"
+RootDirectoryIcons="/usr/share/share/icons"
+RootDirectoryFonts="/usr/share/share/fonts"
