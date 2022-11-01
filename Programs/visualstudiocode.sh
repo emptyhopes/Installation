34cbfab3bc1ce9c9a405da7d0f48bc877dc9bf0d
emptyhopes <literallymyfault@gmail.com>
2022-11-01 08:20:04 +0000

    base

--- Programs/visualstudiocode.sh
+++ Programs/visualstudiocode.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 temporary=$(mktemp --directory)
+
 curl --silent --location "https://az764295.vo.msecnd.net/stable/d045a5eda657f4d7b676dedbfa7aab8207f8a075/code_1.72.2-1665614327_amd64.deb" >"$temporary/visualstudiocode.deb"
+
 sudo apt install "$temporary/visualstudiocode.deb" --yes
+
 echo "Install visual studio code: Success."
