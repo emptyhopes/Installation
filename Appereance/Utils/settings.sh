8915e36165452307b0960565f542d4cbe58823a0
emptyhopes <literallymyfault@gmail.com>
2022-11-01 08:42:27 +0000

    base

--- Appereance/Utils/settings.sh
+++ Appereance/Utils/settings.sh
@@ -8,6 +8,7 @@ fi
 source "/home/$(whoami)/Downloads/installation/variables.sh"
 
 # System
+
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent "true"
 gsettings set org.gnome.desktop.interface clock-show-date "true"
@@ -19,6 +20,7 @@ gsettings set org.gnome.mutter center-new-windows "true"
 echo "Settings up system: Success."
 
 # Dash to dock
+
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts "false"
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash "false"
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed "true"
@@ -29,7 +31,20 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size "40"
 
 echo "Settings up dash to dock: Success."
 
+# Terminal
+
+gsettings set org.gnome.Terminal.Legacy.Profile:/:0/ default-size-columns 120
+
+#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" default-size-columns 120
+#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" default-size-rows 30
+#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" background-transparency-percent 30
+#gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" use-theme-colors false
+#gsettings get org.gnome.Terminal.Legacy.Profile:/:0/ font
+
+echo "Settings up terminal: Success."
+
 # Appearance
+
 gsettings set org.gnome.desktop.interface icon-theme "MacOS-Icons"
 gsettings set org.gnome.desktop.interface gtk-theme "MacOS-Themes"
 gsettings set org.gnome.shell.extensions.user-theme name "MacOS-Themes"
