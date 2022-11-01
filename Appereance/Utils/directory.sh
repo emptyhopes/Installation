87e49449ff514441ec71c8c5f86d2154ba84dc05
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-01 14:59:49 +0000

    Update directory.sh

--- Appereance/Utils/directory.sh
+++ Appereance/Utils/directory.sh
@@ -7,28 +7,20 @@ fi
 
 source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
-ClearDirectory() {
+# Local
+sudo rm --recursive --force "$LocalDirectoryExtensions" | echo "Clear $LocalDirectoryExtensions: Success."
+sudo rm --recursive --force "$LocalDirectoryBackgrounds" | echo "Clear $LocalDirectoryBackgrounds: Success."
+sudo rm --recursive --force "$LocalDirectoryThemes" | echo "Clear $LocalDirectoryThemes: Success."
+sudo rm --recursive --force "$LocalDirectoryCursors" | echo "Clear $LocalDirectoryCursors: Success."
+sudo rm --recursive --force "$LocalDirectoryIcons" | echo "Clear $LocalDirectoryIcons: Success."
+sudo rm --recursive --force "$LocalDirectoryFonts" | echo "Clear $LocalDirectoryFonts: Success."
 
-   # Local
+# Root
+sudo rm --recursive --force "$RootDirectoryExtensions" | echo "Clear $RootDirectoryExtensions: Success."
+sudo rm --recursive --force "$RootDirectoryBackgrounds" | echo "Clear $RootDirectoryBackgrounds: Success."
+sudo rm --recursive --force "$RootDirectoryThemes" | echo "Clear $RootDirectoryThemes: Success."
+sudo rm --recursive --force "$RootDirectoryCursors" | echo "Clear $RootDirectoryCursors: Success."
+sudo rm --recursive --force "$RootDirectoryIcons" | echo "Clear $RootDirectoryIcons: Success."
+sudo rm --recursive --force "$RootDirectoryFonts" | echo "Clear $RootDirectoryFonts: Success."
 
-   sudo rm --recursive --force "$LocalDirectoryExtensions" | echo "Clear $LocalDirectoryExtensions: Success."
-   sudo rm --recursive --force "$LocalDirectoryBackgrounds" | echo "Clear $LocalDirectoryBackgrounds: Success."
-   sudo rm --recursive --force "$LocalDirectoryThemes" | echo "Clear $LocalDirectoryThemes: Success."
-   sudo rm --recursive --force "$LocalDirectoryCursors" | echo "Clear $LocalDirectoryCursors: Success."
-   sudo rm --recursive --force "$LocalDirectoryIcons" | echo "Clear $LocalDirectoryIcons: Success."
-   sudo rm --recursive --force "$LocalDirectoryFonts" | echo "Clear $LocalDirectoryFonts: Success."
-
-   # Root
-
-   cd "$RootDirectoryThemes" | ls | grep (-v) --value Default | xargs sudo rm --recursive --force
-
-   sudo rm --recursive --force "$RootDirectoryExtensions" | echo "Clear $RootDirectoryExtensions: Success."
-   sudo rm --recursive --force "$RootDirectoryBackgrounds" | echo "Clear $RootDirectoryBackgrounds: Success."
-   sudo rm --recursive --force "$RootDirectoryThemes" | echo "Clear $RootDirectoryThemes: Success."
-   sudo rm --recursive --force "$RootDirectoryCursors" | echo "Clear $RootDirectoryCursors: Success."
-   sudo rm --recursive --force "$RootDirectoryIcons" | echo "Clear $RootDirectoryIcons: Success."
-   sudo rm --recursive --force "$RootDirectoryFonts" | echo "Clear $RootDirectoryFonts: Success."
-
-}
-
-ClearDirectory
+# cd "$RootDirectoryThemes" | ls | grep (-v) --value Default | xargs sudo rm --recursive --force
