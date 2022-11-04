e324836e54d2a14f971c815c315d200dd3ba7840
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:27:47 +0000

    Update cursors.sh

--- Appereance/Desktop/cursors.sh
+++ Appereance/Desktop/cursors.sh
@@ -8,8 +8,8 @@ fi
 source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
 if [[ -d "$CursorsDirectory" ]]; then
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/icons/MacOS-Cursors" "$CursorsDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/icons/MacOS-Cursors" "$CursorsDirectory"
 else
    mkdir "$CursorsDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/icons/MacOS-Cursors" "$CursorsDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/icons/MacOS-Cursors" "$CursorsDirectory"
 fi
