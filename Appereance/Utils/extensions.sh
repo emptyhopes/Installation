a14213eba25638140bea03f6534d1de0b485d572
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:11:36 +0000

    Update extensions.sh

--- Appereance/Utils/extensions.sh
+++ Appereance/Utils/extensions.sh
@@ -7,30 +7,8 @@ fi
 
 source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
-DownloadExtensions() {
-   BaseURL="https://extensions.gnome.org"
-   GnomeVersion=$(gnome-shell --version | cut --delimiter=" " --fields="3")
-   extensions=("user-theme@gnome-shell-extensions.gcampax.github.com" "dash-to-dock@micxgx.gmail.com" "blur-my-shell@aunetx")
-
-   for extension in ${extensions[@]}; do
-      temporary=$(mktemp --directory)
-      destination="$LocalDirectoryExtensions/$extension"
-      url=$(GetDownloadURLExtension "$extension")
-
-      curl --silent --location "$url" >"$temporary/$extension.zip"
-
-      if [[ -d "$ExtensionsDirectory" ]]; then
-         unzip -q "$temporary/$extension.zip" -d "$destination"
-      else
-         mkdir "$ExtensionsDirectory"
-         unzip -q "$temporary/$extension.zip" -d "$destination"
-      fi
-
-      sudo rm --recursive --force "$temporary"
-
-      echo "Install $extension: Success."
-   done
-}
+BaseURL="https://extensions.gnome.org"
+GnomeVersion=$(gnome-shell --version | cut --delimiter=" " --fields="3")
 
 GetInformationExtension() {
    echo "$BaseURL/extension-info/?uuid=${1}&shell_version=$GnomeVersion"
@@ -40,4 +18,21 @@ GetDownloadURLExtension() {
    echo "$BaseURL$(curl --silent --location "$(GetInformationExtension $1)" | sed --expression 's/.*"download_url": "\([^"]*\)".*/\1/')"
 }
 
-DownloadExtensions
+for extension in ${Extensions[@]}; do
+   temporary=$(mktemp --directory)
+   destination="$LocalDirectoryExtensions/$extension"
+   url=$(GetDownloadURLExtension "$extension")
+
+   curl --silent --location "$url" >"$temporary/$extension.zip"
+
+   if [[ -d "$ExtensionsDirectory" ]]; then
+      unzip -q "$temporary/$extension.zip" -d "$destination"
+   else
+      mkdir "$ExtensionsDirectory"
+      unzip -q "$temporary/$extension.zip" -d "$destination"
+   fi
+
+   sudo rm --recursive --force "$temporary"
+
+   echo "Install $extension: Success."
+done
