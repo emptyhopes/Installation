dc0718c4fc8621d7db9b0d63a1772a67f43c003c
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:34:41 +0000

    Update icons.sh

--- Appereance/Desktop/icons.sh
+++ Appereance/Desktop/icons.sh
@@ -5,7 +5,7 @@ if [[ "$(whoami)" == "root" ]]; then
    exit 1
 fi
 
-source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
+source "../../Variables/variables.sh"
 
 if [[ -d "$IconsDirectory" ]]; then
    cp --recursive "$InstallationDirectory/Source/home/.local/share/icons/MacOS-Icons" "$IconsDirectory"
