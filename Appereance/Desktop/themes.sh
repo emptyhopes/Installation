c7845ca99d4aaca7a2fd6bb1b2bcced26e2aecd5
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:29:31 +0000

    Update themes.sh

--- Appereance/Desktop/themes.sh
+++ Appereance/Desktop/themes.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!  /bin/bash
 
 if [[ "$(whoami)" == "root" ]]; then
    echo "Dont use super user."
@@ -8,12 +8,12 @@ fi
 source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
 if [[ -d "$ThemesDirectory" ]]; then
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes" "$ThemesDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes-hdpi" "$ThemesDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes-xhdpi" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes-hdpi" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes-xhdpi" "$ThemesDirectory"
 else
    mkdir "$ThemesDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes" "$ThemesDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes-hdpi" "$ThemesDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/themes/MacOS-Themes-xhdpi" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes-hdpi" "$ThemesDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/themes/MacOS-Themes-xhdpi" "$ThemesDirectory"
 fi
