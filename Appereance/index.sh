9046bcce9bfaf8742c7ac52d471373195bbff1bc
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:50:09 +0000

    Create index.sh

---
+++ Appereance/index.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [[ "$(whoami)" == "root" ]]; then
+   echo "Dont use super user."
+   exit 1
+fi
+
+source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
+
+bash "$InstallationDirectory/Appereance/Utils/directory.sh"
+bash "$InstallationDirectory/Appereance/Utils/extensions.sh"
+
+bash "$InstallationDirectory/Appereance/Browser/firefox.sh"
+
+bash "$InstallationDirectory/Appereance/Desktop/themes.sh"
+bash "$InstallationDirectory/Appereance/Desktop/backgrounds.sh"
+bash "$InstallationDirectory/Appereance/Desktop/cursors.sh"
+bash "$InstallationDirectory/Appereance/Desktop/icons.sh"
+bash "$InstallationDirectory/Appereance/Desktop/fonts.sh"
+
+bash "$InstallationDirectory/Appereance/Utils/settings.sh"
