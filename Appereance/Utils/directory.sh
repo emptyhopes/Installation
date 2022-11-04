f4c61bb040a146c6545098f14749d4c954e528ed
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:43:12 +0000

    Update directory.sh

--- Appereance/Utils/directory.sh
+++ Appereance/Utils/directory.sh
@@ -5,7 +5,7 @@ if [[ "$(whoami)" == "root" ]]; then
    exit 1
 fi
 
-source "../../Variables/variables.sh"
+source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
 # Local
 sudo rm --recursive --force "$LocalDirectoryExtensions" | echo "Clear $LocalDirectoryExtensions: Success."
