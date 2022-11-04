185555eb0967e5344d33871791de0cdcc4019ea9
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 05:27:19 +0000

    Update backgrounds.sh

--- Appereance/Desktop/backgrounds.sh
+++ Appereance/Desktop/backgrounds.sh
@@ -8,8 +8,8 @@ fi
 source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
 
 if [[ -d "$BackgroundsDirectory" ]]; then
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/backgrounds/MacOS-Backgrounds" "$BackgroundsDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/backgrounds/MacOS-Backgrounds" "$BackgroundsDirectory"
 else
    mkdir "$BackgroundsDirectory"
-   cp --recursive "/home/$user/Downloads/installation/Source/home/.local/share/backgrounds/MacOS-Backgrounds" "$BackgroundsDirectory"
+   cp --recursive "$InstallationDirectory/Source/home/.local/share/backgrounds/MacOS-Backgrounds" "$BackgroundsDirectory"
 fi
