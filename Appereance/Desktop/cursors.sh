7808574b38ccf53b17c17fdab4b5db03fc71f873
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-04 08:52:38 +0000

    Update cursors.sh

--- Appereance/Desktop/cursors.sh
+++ Appereance/Desktop/cursors.sh
@@ -5,7 +5,12 @@ if [[ "$(whoami)" == "root" ]]; then
    exit 1
 fi
 
-source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
+if [[ -f "/home/$(whoami)/Downloads/installation/Variables/variables.sh" ]]; then
+   source "/home/$(whoami)/Downloads/installation/Variables/variables.sh"
+else
+   echo "File variables not found."
+   exit 1
+fi
 
 if [[ -d "$CursorsDirectory" ]]; then
    cp --recursive "$InstallationDirectory/Source/home/.local/share/icons/MacOS-Cursors" "$CursorsDirectory"
