1a6b96b449031d6b03ee931af6ed2914ff5b64f5
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-05 10:00:05 +0000

    Update gnome.sh

--- Startup/gnome.sh
+++ Startup/gnome.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install gnome-session gnome-terminal --yes
+sudo apt-get install gdm3 gnome-session gnome-shell gnome-shell-extensions gnome-shell-extension-dashtodock gnome-tweaks nautilus gnome-text-editor gnome-calculator gnome-system-monitor gnome-terminal evince kali-desktop-core --yes
 
 sudo dpkg-reconfigure gdm3
 
