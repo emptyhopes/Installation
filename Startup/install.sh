aa117ed11495a42c99f96838bce6c4b30cfaecf4
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-06 08:42:42 -0800

    Update install.sh

--- Startup/install.sh
+++ Startup/install.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install git curl fonts-firacode --yes
 sudo apt-get install gnome-tweaks gnome-shell-extensions gnome-text-editor gdisk nautilus --yes
-sudo apt-get install node npm nginx --yes
+sudo apt-get install nodejs npm nginx --yes
