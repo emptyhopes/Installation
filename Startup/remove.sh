555ed34a06e293f535f0b3a27099a5b04f2934a2
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-09 06:31:39 -0500

    Delete remove.sh

--- Startup/remove.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo apt-get purge --autoremove kali-desktop-xfce --yes
-sudo apt-get purge --autoremove kali-tools-top10 xfce4-* atril cherrytree qterminal onboard engrampa gparted pulseaudio pulseaudio-utils hwloc thunar thunar-data im-config king-phisher gnome-user-docs yelp vim --yes
