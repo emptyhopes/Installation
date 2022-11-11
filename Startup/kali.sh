8bdc7273584d92ebb21203a6ab5487e260b11671
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-11 02:47:34 +0000

    Update kali.sh

--- Startup/kali.sh
+++ Startup/kali.sh
@@ -6,7 +6,6 @@ sudo apt-get install kali-desktop-core gdm3 gnome-session gnome-shell gnome-shel
 sudo apt-get install curl fonts-firacode --yes
 
 sudo apt-get purge --autoremove kali-desktop-xfce xfce4 xfce4-* lightdm kali-tools-top10 --yes
-sudo apt-get purge --autoremove qterminal qterminal-* cherrytree bully bulk-extractor aircrack-ng arping arp-scan cewl chntpw cherrytree commix crunch clang clang-* dmitry dirb dirbuster dns2tcp dnschef dnsenum enum4linux evil-winrm --yes
-
+sudo apt-get purge --autoremove qterminal qterminal-* cherrytree bully bulk-extractor aircrack-ng arping arp-scan cewl chntpw cherrytree commix crunch clang clang-* dmitry dirb dirbuster dns2tcp dnschef dnsenum enum4linux evil-winrm crackmapexec dbd dnsrecon exe2hexbat faraday faraday-* ffuf fierce fping gparted gparted-* hydra hydra-* hashdeep sbd samdump2 rsmangler responder responder recon-ng recordmydesktop rebind radare2 pwnat ptunnel tcpreplay sslh sslscan sslsplit sslyze swaks thc-pptp-bruter theharvester weevely wfuzz whatweb wireshark wireshark-* wordlists --yes
 
 sudo service gdm3 start
