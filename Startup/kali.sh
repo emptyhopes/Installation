145b9fed7db8c164aab49dfcae671aea75cccc2a
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-11 02:17:54 +0000

    Update kali.sh

--- Startup/kali.sh
+++ Startup/kali.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update --yes
 
-sudo apt-get install kali-desktop-core gdm3 gnome-session gnome-shell gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-terminal gnome-tweaks nautilus gnome-text-editor gnome-calculator --yes
+sudo apt-get install kali-desktop-core gdm3 gnome-session gnome-shell gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-terminal gnome-tweaks nautilus gnome-text-editor gnome-calculator gnome-bluetooth --yes
 sudo apt-get install curl fonts-firacode --yes
 
 sudo apt-get purge --autoremove kali-desktop-xfce xfce4 xfce4-* lightdm kali-tools-top10 kali-menu --yes
