fbfb156e2b16dfcf0fe960adc4a85472fddf03ee
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-13 13:30:34 -0500

    Update gnome.sh

--- Startup/gnome.sh
+++ Startup/gnome.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get update --yes
 
-sudo apt-get install kali-desktop-core gdm3 gnome-session gnome-shell gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-terminal gnome-tweaks nautilus
+sudo apt-get install gdm3 gnome-session gnome-shell gnome-shell-extensions gnome-shell-extension-appindicator gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-terminal gnome-tweaks nautilus
 
 sudo update-alternatives --config x-session-manager
 sudo update-alternatives --config x-session-manager
