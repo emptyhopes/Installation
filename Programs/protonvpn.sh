82250039dd8470b3c1a5f27ba98ee6b6bdcda69e
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-14 01:15:44 -0500

    Update protonvpn.sh

--- Programs/protonvpn.sh
+++ Programs/protonvpn.sh
@@ -7,6 +7,3 @@ curl --silent --location "https://repo.protonvpn.com/debian/dists/stable/main/bi
 sudo apt install "$temporary/protonvpn.deb" --yes
 sudo apt-get update
 sudo apt-get install protonvpn --yes
-sudo apt-get install gnome-shell-extension-appindicator --yes
-
-grep '/usr/s\?bin' /etc/systemd/system/display-manager.service
