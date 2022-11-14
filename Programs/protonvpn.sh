c2352fd76db953c8cd35705e7f59bafad7dffa94
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-14 01:02:09 -0500

    Update protonvpn.sh

--- Programs/protonvpn.sh
+++ Programs/protonvpn.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 sudo apt-get install protonvpn
-sudo apt-get install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1
+sudo apt-get install gnome-shell-extension-appindicator
 
 grep '/usr/s\?bin' /etc/systemd/system/display-manager.service
