559385a1adc3ccdaf19a2b4dc5d751a78696510f
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-14 01:17:30 -0500

    Update packages.sh

--- Startup/packages.sh
+++ Startup/packages.sh
@@ -5,3 +5,4 @@
 sudo apt-get install curl fonts-firacode --yes
 sudo apt-get install gnome-tweaks gnome-shell-extensions --yes
 sudo apt-get install nodejs npm nginx --yes
+sudo apt-get install net-tools --yes
