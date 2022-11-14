f0b04f2eac3f4360a7189de8aeaed57a8682abf6
emptyhopes <67724195+emptyhopes@users.noreply.github.com>
2022-11-14 01:10:52 -0500

    Update protonvpn.sh

--- Programs/protonvpn.sh
+++ Programs/protonvpn.sh
@@ -5,6 +5,7 @@ temporary=$(mktemp --directory)
 curl --silent --location "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb" >"$temporary/protonvpn.deb"
 
 sudo apt install "$temporary/protonvpn.deb" --yes
+sudo apt-get update
 sudo apt-get install protonvpn --yes
 sudo apt-get install gnome-shell-extension-appindicator --yes
 
